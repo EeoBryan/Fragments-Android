db922c801d4db51160f984410a04fd53f6eebe46
Bryan <bryan.eeo2@gmail.com>
2019-11-25 11:24:55 +0800

    Added Fragment, RecyclerView and CardView

---
+++ app/src/main/java/com/msssolution/convostartertest2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.msssolution.convostartertest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragment {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/msssolution/convostartertest2/SingleFragment.java
@@ -0,0 +1,28 @@
+package com.msssolution.convostartertest2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragment extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
