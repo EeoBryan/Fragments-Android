ee86ba48dbaca13b376c6130eb35bb17693c87c1
Bryan <bryan.eeo2@gmail.com>
2019-11-25 11:55:00 +0800

    Added RecyclerView Adapter and ViewHolder

--- app/src/main/java/com/msssolution/convostartertest2/RecyclerFragment.java
+++ app/src/main/java/com/msssolution/convostartertest2/RecyclerFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,7 +26,45 @@ public class RecyclerFragment extends Fragment {
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new RecyclerViewAdapter());
 
         return view;
+    }
+
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+        private TextView titleTextView, descTextView;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public RecyclerViewHolder(LayoutInflater inflater, ViewGroup container) {
+            super(inflater.inflate(R.layout.card_view, container, false));
+            cardView = itemView.findViewById(R.id.card_container);
+            titleTextView = itemView.findViewById(R.id.item_title_text_view);
+
+        }
+    }
+
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+        @NonNull
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+            return new RecyclerViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
     }
 }
