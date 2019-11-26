cb636c6fa216855af19f55601b51d169d668973c
Bryan <bryan.eeo2@gmail.com>
2019-11-26 12:37:52 +0800

    Added CSV class

--- app/src/main/java/com/msssolution/convostartertest2/RecyclerFragment.java
+++ app/src/main/java/com/msssolution/convostartertest2/RecyclerFragment.java
@@ -5,14 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
 
